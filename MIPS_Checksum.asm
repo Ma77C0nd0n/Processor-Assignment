c5daf5f072ea0cba602782e4e10a29d74d011fec
Ma77C0nd0n <matt.condon@ucdconnect.ie>
2015-11-22 22:48:22 +0000

    Changed stuff
    Put more stuff in the report if you like

--- MIPS_Checksum.asm
+++ MIPS_Checksum.asm
@@ -6,23 +6,30 @@ Stu: 	.word 1,3,5,2,3,1,0,7 # individual digits are stored in word
 	.globl main 
 main:	la $t0, Stu
 
-Loop:	lw $t1, 0($t0)		# 00 10 00 01 = 0x21
-	add $t2, $t1, $t2	# 11 11 10 11 = 0xFB
-	lw $t1, 4($t0)		# 00 
-	add $t2, $t1, $t2
-	lw $t1, 8($t0)		#
-	add $t2, $t1, $t
-	lw $t1, 12($t0)		#
-	add $t2, $t1, $t2
-	lw $t1, 16($t0)		#
-	add $t2, $t1, $t2
-	lw $t1, 20($t0)		#
-	add $t2, $t1, $t2
-	lw $t1, 24($t0)		#
-	add $t2, $t1, $t2
-	lw $t1, 28($t0)		#
-	add $t2, $t1, $t2
+Loop:	lw $t1, 0($t0)		# 00 01 10 01 = 0x18
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95 (01 added for circuit)
+	lw $t2, 0($t0)		# 00 01 11 00 = 0x1C
+	add $t2, $t1, $t2	# 11 10 11 11 = 0xEF
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95
+	lw $t1, 0($t0)		# 00 01 10 01 = 0x18
+	add $t2, $t1, $t2	# 11 10 11 11 = 0xEF
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95
+	lw $t1, 0($t0)		# 00 01 10 01 = 0x18
+	add $t2, $t1, $t2	# 11 10 11 11 = 0xEF
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95
+	lw $t1, 0($t0)		# 00 01 10 01 = 0x18
+	add $t2, $t1, $t2	# 11 10 11 11 = 0xEF
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95
+	lw $t1, 0($t0)		# 00 01 10 01 = 0x18
+	add $t2, $t1, $t2	# 11 10 11 11 = 0xEF
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95
+	lw $t1, 0($t0)		# 00 01 10 01 = 0x18
+	add $t2, $t1, $t2	# 11 10 11 11 = 0xEF
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95
+	lw $t1, 0($t0)		# 00 01 10 00 = 0x18
+	add $t2, $t1, $t2	# 11 10 11 11 = 0xEF
+	addi $t0, $t0, 04	# 10 01 01 01 = 0x95
 	
-  	sw $t2, 32($t0)		#		
+  	sw $t2, 0($t0)		# 01 01	11 00 = 0x5C
 	li $v0, 10
 	syscall 
